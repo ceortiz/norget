861507e8697c5b2ffefdc4aa0868b819e7213007
Carl Ortiz <ceortiz@up.edu.ph>
2020-05-30 21:04:34 +0800

    w/ Trigram Similarity for getting related news

--- news/urls.py
+++ news/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	path('post_news', views.post_news, name='post_news'),
 	path('get_data', views.get_data, name='get_data'),
 	path('post_headline', views.post_headline,name='post_headline'),
-	path('other_perspectives', views.other_perspectives,name='other_perspectives'),
+	#path('other_perspectives', views.other_perspectives,name='other_perspectives'),
 	path('memory', views.memory, name='memory'),
 	path('headlines', views.headlines_selection, name='headlines'),
 ]
